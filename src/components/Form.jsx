a3d0fb55ded6d298a010aadb0ebec1420711577a
jucemalco <jucemalco@gmail.com>
2022-02-08 22:03:44 +0000

    last update fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import Todoitem from "./Todoitem";
 
 const Form = () => {
   const [task, setTask] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([{}]);
 
   const handleChange = (event) => {
     setTask(event.target.value);
@@ -11,10 +11,12 @@ const Form = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    setList([...list, task]);
+    setList((prev)=>{return[...prev,{label:task,done:false}]})
     setTask("");
   };
-
+  let obtener = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/jucemalco").then((response) => response.json()).then((data) => setList(data))
+}
   const onDelete = (item) => {
     const filteredTodoList = list.filter((todoItem) => {
       return todoItem !== item;
@@ -22,7 +24,20 @@ const Form = () => {
 
     setList(filteredTodoList);
   };
-
+  let actualizar = () => {
+  fetch("https://assets.breatheco.de/apis/fake/todos/user/jucemalco", {
+  "method": "PUT",
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "body": JSON.stringify(list)
+})
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.error(err);
+});}
   return (
     <div className="form">
       <h1 className="title">TAREAS PARA HOY :D</h1>
@@ -34,10 +49,12 @@ const Form = () => {
           onChange={handleChange}
         />
       </form> 
+      <button type="button" className="btn btn-primary" onClick={obtener}>Listar GUARDADO</button>
+      <button type="button" className="btn btn-primary" onClick={actualizar}>Actualizar DATOS</button>
       <ul>
         {list.map((item, index) => {
           console.log(item);
-          return <Todoitem todoitem={item} deleteTodo={onDelete} key={index} />;
+          return <Todoitem todoitem={item.label} deleteTodo={onDelete} key={index} />;
         })}
       </ul>
     </div>
